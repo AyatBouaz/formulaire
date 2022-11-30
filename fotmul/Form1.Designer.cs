c8ed9ea1a46db8b8489ca043f88df9aa936bf699
dell <dell@DESKTOP-OFLO969>
2022-11-30 20:53:54 +0100

    Add project files.

---
+++ fotmul/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace fotmul
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ajouter = new System.Windows.Forms.Button();
+            this.afficher = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.modifier = new System.Windows.Forms.Button();
+            this.annuler = new System.Windows.Forms.Button();
+            this.supprimer = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ajouter
+            // 
+            this.ajouter.Location = new System.Drawing.Point(57, 294);
+            this.ajouter.Name = "ajouter";
+            this.ajouter.Size = new System.Drawing.Size(75, 23);
+            this.ajouter.TabIndex = 0;
+            this.ajouter.Text = "ajouter";
+            this.ajouter.UseVisualStyleBackColor = true;
+            this.ajouter.Click += new System.EventHandler(this.ajouter_Click);
+            // 
+            // afficher
+            // 
+            this.afficher.Location = new System.Drawing.Point(163, 294);
+            this.afficher.Name = "afficher";
+            this.afficher.Size = new System.Drawing.Size(75, 23);
+            this.afficher.TabIndex = 1;
+            this.afficher.Text = "afficher";
+            this.afficher.UseVisualStyleBackColor = true;
+            this.afficher.Click += new System.EventHandler(this.afficher_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(193, 348);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(8, 8);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // modifier
+            // 
+            this.modifier.Location = new System.Drawing.Point(285, 294);
+            this.modifier.Name = "modifier";
+            this.modifier.Size = new System.Drawing.Size(75, 23);
+            this.modifier.TabIndex = 3;
+            this.modifier.Text = "modifier";
+            this.modifier.UseVisualStyleBackColor = true;
+            this.modifier.Click += new System.EventHandler(this.modifier_Click);
+            // 
+            // annuler
+            // 
+            this.annuler.Location = new System.Drawing.Point(400, 294);
+            this.annuler.Name = "annuler";
+            this.annuler.Size = new System.Drawing.Size(75, 23);
+            this.annuler.TabIndex = 4;
+            this.annuler.Text = "annuler";
+            this.annuler.UseVisualStyleBackColor = true;
+            this.annuler.Click += new System.EventHandler(this.annuler_Click);
+            // 
+            // supprimer
+            // 
+            this.supprimer.Location = new System.Drawing.Point(440, 130);
+            this.supprimer.Name = "supprimer";
+            this.supprimer.Size = new System.Drawing.Size(75, 23);
+            this.supprimer.TabIndex = 5;
+            this.supprimer.Text = "supprimer";
+            this.supprimer.UseVisualStyleBackColor = true;
+            this.supprimer.Click += new System.EventHandler(this.supprimer_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(57, 110);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(282, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(57, 170);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(282, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(521, 206);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.supprimer);
+            this.Controls.Add(this.annuler);
+            this.Controls.Add(this.modifier);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.afficher);
+            this.Controls.Add(this.ajouter);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ajouter;
+        private System.Windows.Forms.Button afficher;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button modifier;
+        private System.Windows.Forms.Button annuler;
+        private System.Windows.Forms.Button supprimer;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ fotmul/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace fotmul
+{
+    public partial class Form1 : Form
+    {
+        static string chaine = @"Data Source=DESKTOP-OFLO969\SQLEXPRESS;Initial Catalog=biblio;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        //"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\App_Data\VotreBD.mdf;Integrated Security=True;User Instance=True"
+        //"Server=.\SQLEXPRESS; DataBase=VotreBD;USER ID=sa; PASSWORD="
+        static SqlConnection cnx = new SqlConnection(chaine);
+        static SqlCommand cmd = new SqlCommand();
+        static SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            annuler.Enabled = false;
+            afficher.Enabled = false;
+            modifier.Enabled = false;
+            ajouter.Enabled = true;
+            supprimer.Enabled = false;
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void ajouter_Click(object sender, EventArgs e)
+        {
+            cnx.Open();
+            cmd.Connection = cnx;
+            cmd.CommandText = "insert into aya values('" + textBox1.Text + "','" + textBox2.Text + "') ";
+            cmd.ExecuteNonQuery();
+            cnx.Close();
+            textBox1.Clear();
+            textBox2.Clear();
+
+            annuler.Enabled = true;
+            afficher.Enabled = true;
+            modifier.Enabled = true;
+            ajouter.Enabled = true;
+            supprimer.Enabled = false;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void afficher_Click(object sender, EventArgs e)
+        {
+            cnx.Open();
+            cmd.CommandText = "select * from aya ";
+            cmd.Connection = cnx;
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            cnx.Close();
+        }
+
+        private void modifier_Click(object sender, EventArgs e)
+        {
+            cmd.CommandText = "update aya set nom ='" + textBox1.Text + "' where prenom='" + textBox2.Text + "' ";
+            cmd.ExecuteNonQuery();
+            cnx.Close();
+        }
+
+        private void annuler_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+
+            annuler.Enabled = false;
+            modifier.Enabled = false;
+            ajouter.Enabled = true;
+            supprimer.Enabled = false;
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+        }
+
+        private void supprimer_Click(object sender, EventArgs e)
+        {
+            string box_msg = "confirmer la suppression";
+
+            string box_title = "confiramtion";
+
+
+
+            if (MessageBox.Show(box_msg, box_title, MessageBoxButtons.YesNo) == DialogResult.Yes)
+
+            {
+                if (cnx.State == ConnectionState.Open)
+                {
+                    cnx.Close();
+                }
+
+                cnx.Open();
+                if (cmd.Connection.State == ConnectionState.Open)
+                {
+                    cmd.Connection.Close();
+                }
+                cnx.Open();
+                cmd.Connection = cnx;
+                cmd.CommandText = "delete from aya where nom='" + textBox1.Text + "' ";
+                cmd.ExecuteNonQuery();
+                cnx.Close();
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+
+
+    }
+}
+}
+
